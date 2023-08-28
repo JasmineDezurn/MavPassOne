f5ed488791aea4204566f4c4272de23a97ae360e
JasmineDezurn <143098244+JasmineDezurn@users.noreply.github.com>
2023-08-28 13:47:43 -0500

    Add files via upload

---
+++ CISMavPass.py
@@ -0,0 +1,6 @@
+# My first Python Program
+
+name=input("What is your name?")
+
+print("Hello world!")
+print("My name is Jasmine Dezurn. I will be going for a computer science degree. I enjoying cooking and dancing, and I own a tiara.")
